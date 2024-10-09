42e1974b5fdb19113940b877052a42cc7f560b94
Lameed88 <moradeyor88@gmail.com>
2024-10-09 18:18:00 +0100

    update

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    ssg: false // means build time the pages wont generate static html, only runtime with ISR
 };
 
 module.exports = {
