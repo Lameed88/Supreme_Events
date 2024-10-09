f43794153dc573e75276e3b6d19095a063f77835
Lameed88 <moradeyor88@gmail.com>
2024-10-09 19:30:03 +0100

    update

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 
 const cors = require("cors");
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
